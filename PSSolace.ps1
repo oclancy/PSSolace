63c80d581baff57ae9da6b9664f4f6a5f1565944
oclancy <oclancy@gmail.com>
2021-11-17 11:28:22 +0000

    Update PSSolace.ps1

--- PSSolace.ps1
+++ PSSolace.ps1
@@ -1,36 +1,66 @@
 using namespace SolaceSystems.Solclient.Messaging
+using namespace Proquote.TechnologyPlatform.Service.Endpoints.Solace.Messaging
 
 Add-Type -AssemblyName "${PSScriptRoot}\\SolaceSystems.Solclient.Messaging.dll" -PassThru
+Add-Type -AssemblyName "${PSScriptRoot}\\Newtonsoft.Json.dll" -PassThru
+Add-Type -AssemblyName "${PSScriptRoot}\\Firmus.Messages.dll" -PassThru
+Add-Type -AssemblyName "${PSScriptRoot}\\Protobuf.dll" -PassThru
 
-function SendMessage {
-    
+function Send-SolaceMessageToTopic{
+
     [CmdletBinding()]
     param (
-        [Parameter()]
         [ISession]
         $Session,
-        [Parameter()]
-        [string]
-        $Destination,
-        [Parameter()]
-        [string]
+        [IFirmusMessage]
         $Message
     )
 
-    [IMessage] $message = [ContextFactory]::Instance.CreateMessage()
+    $serialiser = New-Object "Serialiser<FirmusMessage>" #TBD - Make generic....
 
-    $message.top = $Destination;
-    $message.DeliveryMode = [MessageDeliveryMode]::NonPersistent;
-    $message.BinaryAttachment = Encoding.ASCII.GetBytes($Message);
+    [IMessage] $solacemessage = [ContextFactory]::Instance.CreateMessage()
 
-    Write-Host "Sending message to queue {$Destination}..."
-    $returnCode = $Session.Send($message);
+    $solacemessage.Destination = [ContextFactory]::Instance.CreateTopic( $Message.Destination.Name );
 
+    $solacemessage.DeliveryMode = [MessageDeliveryMode]::Persistent
+    $solacemessage.BinaryAttachment = $serialiser.Serialise($Message)
+
+    Write-Host "Sending message to topic {$Topic.Name}..."
+    return $session.Send($solacemessage)
+}
+
+function New-PriceSolaceMessage{
+
+    [CmdletBinding()]
+    param (
+        [string]
+        $Type,
+        [string]
+        $Symbol,
+        [string]
+        $Isin,
+        [string]
+        $Exchange,
+        [double]
+        $Price,
+        [double]
+        $Size,
+        [TradingPhase]
+        $TradingPhase
+    )
+
+    $newprice = New-Object $Type
+    $newprice.StockId = "$Isin/$Exchange/$Symbol"
+    $newprice.TouchSize = $Size
+    $newprice.TouchPrice = $Price
+    $newprice.Currency = $Currency
+    $newprice.TradingPhase = $TradingPhase
+
+    return $newprice
 }
 
 function HandleSessonEvent
 {
-
     [CmdletBinding()]
     param (
         [Parameter()]
@@ -41,80 +71,129 @@ function HandleSessonEvent
         $SessionEventArgs
     )
 
-    Write-Host "Received session event $( SessionEventArgs.ToString )."
+    Write-Host "Received session event $( $SessionEventArgs.ToString )."
 }
 
-
-
-if ($args.Length -lt 3)
+function Set-Price
 {
-    Write-Host "Usage: TopicPublisher <host> <username>@<vpnname> <password>"
-    Environment.Exit(1);
-}
+    [CmdletBinding()]
+    param (
+        [string]
+        $SolaceHost,
+        [string]
+        $VpnName,
+        [string]
+        $User,
+        [string]
+        $Password,
+        [string]
+        $Symbol,
+        [string]
+        $Isin,
+        [string]
+        $Exchange,
+        [double]
+        $Price,
+        [double]
+        $Size,
+        [TradingPhase]
+        $TradingPhase = [TradingPhase]::RegularTrading
+    )
 
-$split = $args[1].Split('@');
-if ($split.Length -ne 2)
-{
-    Write-Host "Usage: TopicPublisher <host> <username>@<vpnname> <password>";
-    [System.Environment]::Exit(1);
-}
-
-$hostname = $args[0]; # Solace messaging router host name or IP address
-$vpnname = $split[1];
-$username = $split[0];
-$password = $args[2];
-
-# Initialize Solace Systems Messaging API with logging to console at Warning level
-$cfp = New-Object -TypeName "ContextFactoryProperties"
-$cfp.SolClientLogLevel = [SolLogLevel]::Warning
-
-$cfp.LogToConsoleError();
-[ContextFactory]::Instance.Init($cfp);
-
-try
-{
-    # Context must be created first
-    $ctxtProps = New-Object -TypeName "ContextProperties"
-    $context = [ContextFactory]::Instance.CreateContext($ctxtProps, $null)
-
-    $sessionProps = New-Object -TypeName "SessionProperties"
-
-    $sessionProps.Host = $hostname
-    $sessionProps.VPNName = $vpnname
-    $sessionProps.UserName = $username
-    $sessionProps.Password = $password
-    $sessionProps.ReconnectRetries = 1 # DefaultReconnectRetries
-
-    $session = $context.CreateSession($sessionProps, $null, $HandleSessionEvent)
-
-    $returnCode = $session.Connect();
-
-    if ($returnCode -eq [ReturnCode]::SOLCLIENT_OK)
+    if ($args.Length -lt 3)
     {
+        Write-Host "Usage: TopicPublisher <host> <username>@<vpnname> <password>"
+        Environment.Exit(1);
+    }
 
-        $topic = [ContextFactory]::Instance.CreateTopic("TopicA");
-        if ($session.Subscribe($topic, $true) == [ReturnCode]::SOLCLIENT_OK)
+    try{
+        $lastPx = New-PriceSolaceMessage "StockPriceMessage" $Symbol $Isin $Exchange $Price $Size $TradingPhase
+        $session = New-Session $SolaceHost $VpnName $User $Password
+        Send-SolaceMessage $session $lastPx
+    }
+    catch
+    {
+        Write-Error "Exception thrown: {$_.Exception.Message}";
+        Write-Error "$([ContextFactory]::GetLastSDKErrorInfo)"
+    }
+    finally{
+        Cleanup-Session
+    }
+}
+
+function New-Session{
+
+  [CmdletBinding()]
+
+    param (
+        [string]
+        $SolaceHost,
+        [string]
+        $VpnName,
+        [string]
+        $User,
+        [string]
+        $Password
+    )
+
+    # Initialize Solace Systems Messaging API with logging to console at Warning level
+    $cfp = New-Object -TypeName "ContextFactoryProperties"
+    $cfp.SolClientLogLevel = [SolLogLevel]::Warning
+
+    $cfp.LogToConsoleError();
+    [ContextFactory]::Instance.Init($cfp);
+
+    try
+    {
+        # Context must be created first
+        $ctxtProps = New-Object -TypeName "ContextProperties"
+        $context = [ContextFactory]::Instance.CreateContext($ctxtProps, $null)
+
+        $sessionProps = New-Object -TypeName "SessionProperties"
+
+        $sessionProps.Host = $SolaceHost
+        $sessionProps.VPNName = $VpnName
+        $sessionProps.UserName = $User
+        $sessionProps.Password = $Password
+        $sessionProps.ReconnectRetries = 1 # DefaultReconnectRetries
+
+        $session = $context.CreateSession($sessionProps, $null, $HandleSessionEvent)
+
+        $returnCode = $session.Connect();
+
+        if ($returnCode -eq [ReturnCode]::SOLCLIENT_OK)
         {
-            Write-Host "Successfully added topic subscription";
+            Write-Host "Session successfully connected."
         }
-
-        Write-Host "Session successfully connected."
-
-        SendMessage($session, "topic1", "testMsg");
+        else
+        {
+            Write-Error "Error connecting, return code: $returnCode"
+        }
     }
-    else
+    catch
     {
-        Write-Error "Error connecting, return code: $returnCode"
+        Write-Error "Exception thrown: {$_.Exception.Message}";
+        Write-Error "$([ContextFactory]::GetLastSDKErrorInfo)"
+        throw
     }
 }
-catch
-{
-    Write-Error "Exception thrown: {$_.Message}";
-    Write-Error "$([ContextFactory]::GetLastSDKErrorInfo)"
+
+function Cleanup-Session{
+    try
+    {
+        # Dispose Solace Systems Messaging API
+        [ContextFactory]::Instance.Cleanup();
+    }
+    catch
+    {
+        Write-Error "Exception thrown: {$_.Exception.Message}";
+        Write-Error "$([ContextFactory]::GetLastSDKErrorInfo)"
+    }
+    finally
+    {
+        Write-Host "Finished."
+    }
+
 }
-finally
-{
-    # Dispose Solace Systems Messaging API
-    [ContextFactory]::Instance.Cleanup();
-}
-Write-Host "Finished."
+
+Set-Price "192.168.1.136" "SOL1" "admin" "admin" "BARC" "GB00B" "L" 123.0 456.0
